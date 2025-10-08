a39216fa39a70f749b910acbcecf72d998c9ad37
Talon-Q <talonq@umich.edu>
2025-10-08 17:37:20 -0400

    Update script.js

--- SP-input-waiting/script.js
+++ SP-input-waiting/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // WAITING ROOM
     const waitingRoom = document.getElementById('waiting-room');
+    
     waitingRoom.addEventListener('submit', function(event) {
         waitingRoom.style.display = 'block';
         event.preventDefault();
