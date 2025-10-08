9375d5e0631c6aee789c220d39bde9e44ca93c14
Talon-Q <talonq@umich.edu>
2025-10-08 17:36:16 -0400

    updated the waiting room

--- SP-input-waiting/script.js
+++ SP-input-waiting/script.js
@@ -1,25 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // USERNAME INPUT
-    const usernameSubmission = document.getElementById('username-submission');
-    const usernameInput = document.getElementById('username-input');
-
     // WAITING ROOM
     const waitingRoom = document.getElementById('waiting-room');
-    const displayUsername = document.getElementById('display-username');
-
-    usernameSubmission.addEventListener('submit', function(event) {
-        // USERNAME INPUT
-        event.preventDefault();
-        const username = usernameInput.value;
-        alert('Username submitted: ' + username);
-        console.log('Username submitted:', username);
-        usernameInput.value = '';
-
-        // WAITING ROOM
-        usernameSubmission.style.display = 'none';
-        displayUsername.textContent = 'Username: ' + username;
+    waitingRoom.addEventListener('submit', function(event) {
         waitingRoom.style.display = 'block';
-        console.log(username + " has entered the waiting room.");
         event.preventDefault();
     });
 });
