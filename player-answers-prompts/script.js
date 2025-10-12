e3f96c778cd8a3228dfc75b25739d555dc8210a2
James Royer <jchristopherroyer@gmail.com>
2025-10-12 15:07:52 -0400

    Sorting, stylizing
    Added some temporary functionality for the presentation tomorrow, fixed some typos, etc etc

--- player-answers-prompts/script.js
+++ player-answers-prompts/script.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.getElementById('submitButton');
     const responseInput = document.getElementById('response-input');
-    
-    submitButton.addEventListener('click', function(event) {
+
+    submitButton.addEventListener('click', function (event) {
         event.preventDefault();
         const response = responseInput.value;
         console.log('Response submitted:', response);
         responseInput.value = '';
+
+        const newLoc = "../response-screen.html";
+        window.location.href = newLoc;
     });
 });
